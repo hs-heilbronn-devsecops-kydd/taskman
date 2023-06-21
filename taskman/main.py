0370cf47afe7105e215795a9cb34f8d9b3805f5f
Kainat-Zeba 18 <kainatzeba960@gmail.com>
2023-06-21 13:38:21 +0000

    fixed import errors

--- taskman/main.py
+++ taskman/main.py
@@ -4,11 +4,6 @@ from typing import List, Optional
 from os import getenv
 from typing_extensions import Annotated
 
-from fastapi import Depends, FastAPI
-from starlette.responses import RedirectResponse
-from .backends import Backend, RedisBackend, MemoryBackend, GCSBackend
-from .model import Task, TaskRequest
-
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
@@ -17,6 +12,11 @@ from opentelemetry.sdk.trace.export import (
 )
 
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+from fastapi import Depends, FastAPI
+from starlette.responses import RedirectResponse
+from .backends import Backend, RedisBackend, MemoryBackend, GCSBackend
+from .model import Task, TaskRequest
 
 app = FastAPI()
 
